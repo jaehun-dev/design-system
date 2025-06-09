e65bbf42e36138f3737cc0b06b2d80f183beb724
Jaehun <qkdwogns98@gmail.com>
2025-06-09 21:10:02 +0900

    add reset.css, global.css (design token)

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 
 import App from './App.tsx'
 
+import './styles/reset.css'
+import './styles/global.css'
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
