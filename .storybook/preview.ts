cabb719ff4ca24853ef5066db5fe1cb99748702f
Jaehun <qkdwogns98@gmail.com>
2025-06-09 21:33:08 +0900

    remove @storybook/addon-onboarding

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,5 +1,8 @@
 import type { Preview } from '@storybook/react-vite'
 
+import '../src/styles/reset.css'
+import '../src/styles/global.css'
+
 const preview: Preview = {
   parameters: {
     controls: {
